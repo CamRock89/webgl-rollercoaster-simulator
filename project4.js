5dc87b869e5574515b64df51f3e1a2a77a6251d9
ASolis17 <anthonysolis1714@gmail.com>
2026-05-08 17:19:24 -0400

    Update project4.js

--- project4.js
+++ project4.js
@@ -2,12 +2,22 @@
 
 var canvas;
 var gl;
+var program;
 
 var pointsArray = [];
 var colorsArray = [];
+var texCoordsArray = [];
 
 var modelView;
 var projection;
+
+var vBuffer;
+var cBuffer;
+var tBuffer;
+
+var terrainTexture;
+var useTextureLoc;
+var useTexture = 0;
 
 var mvMatrix;
 var pMatrix;
@@ -23,26 +33,26 @@ var phi = 45.0 * Math.PI / 180.0;
 
 var dr = 5.0 * Math.PI / 180.0;
 
-var left = -5.0;
-var right = 5.0;
-var ytop = 5.0;
-var bottom = -5.0;
+var left = -15.0;
+var right = 15.0;
+var ytop = 15.0;
+var bottom = -15.0;
 
-var near = -20.0;
-var far = 20.0;
+var near = -50.0;
+var far = 50.0;
+// terrain settings
+var terrainRows = 150;
+var terrainCols = 150;
 
-var terrainRows = 100;
-var terrainCols = 100;
-
-var terrainSize = 8.0;
+var terrainSize = 30.0;
 var terrainData = [];
 
 var terrainStart = 0;
 var terrainCount = 0;
-
+// water settings
 var waterStart = 0;
 var waterCount = 0;
-
+//skybox settings
 var skyboxStart = 0;
 var skyboxCount = 0;
 
@@ -68,25 +78,25 @@ function generateTerrainData(rows, cols)
             var z = terrainSize * j / cols - terrainSize / 2;
 
             var y = 0.0;
-
-            y += hill(x, z, -3.0, -3.0, 1.4, 1.8);
-
+            // large hill
+            y += hill(x, z, -10.0, -10.0, 4.0, 20.0);
+            // two med hills
             y += hill(x, z, 2.4, -3.0, 0.55, 0.8);
             y += hill(x, z, 3.3, -2.3, 0.45, 0.7);
-
-            y += hill(x, z, -3.0, 2.2, 0.45, 0.7);
-            y += hill(x, z, -2.2, 3.0, 0.40, 0.7);
-            y += hill(x, z, -3.5, 3.2, 0.35, 0.6);
-            y += hill(x, z, -2.4, 2.1, 0.30, 0.6);
-
-            y += 0.08 * Math.sin(2.0 * x);
-            y += 0.06 * Math.cos(2.0 * z);
+            // four small hills
+            y += hill(x, z, -10.0, 8.0, 1.5, 7.0);
+            y += hill(x, z, -7.0, 11.0, 1.3, 7.0);
+            y += hill(x, z, -12.0, 11.0, 1.2, 6.0);
+            y += hill(x, z, -7.5, 7.5, 1.1, 6.0);
+            //rolling ground
+            y += 0.18 * Math.sin(0.6 * x);
+            y += 0.14 * Math.cos(0.6 * z);
 
             var distFromCenter = Math.sqrt(x * x + z * z);
 
-            if (distFromCenter < 1.5)
+            if (distFromCenter < 4.0)
             {
-                y = -0.25;
+                y = -0.35;
             }
 
             terrainData[i][j] = y;
@@ -114,31 +124,44 @@ function buildTerrain()
             var y3 = terrainData[i + 1][j + 1];
             var y4 = terrainData[i][j + 1];
 
+            var s1 = i / terrainRows;
+            var s2 = (i + 1) / terrainRows;
+
+            var t1 = j / terrainCols;
+            var t2 = (j + 1) / terrainCols;
+
             var green1 = vec4(0.0, 0.45, 0.0, 1.0);
             var green2 = vec4(0.0, 0.65, 0.0, 1.0);
 
             pointsArray.push(vec4(x1, y1, z1, 1.0));
             colorsArray.push(green1);
+            texCoordsArray.push(vec2(s1, t1));
 
             pointsArray.push(vec4(x2, y2, z1, 1.0));
             colorsArray.push(green2);
+            texCoordsArray.push(vec2(s2, t1));
 
             pointsArray.push(vec4(x2, y3, z2, 1.0));
             colorsArray.push(green1);
+            texCoordsArray.push(vec2(s2, t2));
 
             pointsArray.push(vec4(x1, y1, z1, 1.0));
             colorsArray.push(green1);
+            texCoordsArray.push(vec2(s1, t1));
 
             pointsArray.push(vec4(x2, y3, z2, 1.0));
             colorsArray.push(green2);
+            texCoordsArray.push(vec2(s2, t2));
 
             pointsArray.push(vec4(x1, y4, z2, 1.0));
             colorsArray.push(green1);
+            texCoordsArray.push(vec2(s1, t2));
         }
     }
 
     terrainCount = pointsArray.length - terrainStart;
 }
+
 
 
 function buildWater()
@@ -147,26 +170,32 @@ function buildWater()
 
     var waterColor = vec4(0.0, 0.3, 0.9, 1.0);
 
-    var y = -0.20;
-    var size = 1.6;
+    var y = -0.30;
+    var size = 4.2;
 
     pointsArray.push(vec4(-size, y, -size, 1.0));
     colorsArray.push(waterColor);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(size, y, -size, 1.0));
     colorsArray.push(waterColor);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(size, y, size, 1.0));
     colorsArray.push(waterColor);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(-size, y, -size, 1.0));
     colorsArray.push(waterColor);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(size, y, size, 1.0));
     colorsArray.push(waterColor);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(-size, y, size, 1.0));
     colorsArray.push(waterColor);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     waterCount = pointsArray.length - waterStart;
 }
@@ -179,27 +208,63 @@ function buildSkybox()
     var topBlue = vec4(0.0, 0.1, 0.6, 1.0);
     var bottomBlue = vec4(0.5, 0.8, 1.0, 1.0);
 
-    var size = 12.0;
+    var size = terrainSize * 1.5;
 
     pointsArray.push(vec4(-size, -size, -size, 1.0));
     colorsArray.push(bottomBlue);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(size, -size, -size, 1.0));
     colorsArray.push(bottomBlue);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(size, size, -size, 1.0));
     colorsArray.push(topBlue);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(-size, -size, -size, 1.0));
     colorsArray.push(bottomBlue);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(size, size, -size, 1.0));
     colorsArray.push(topBlue);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     pointsArray.push(vec4(-size, size, -size, 1.0));
     colorsArray.push(topBlue);
+    texCoordsArray.push(vec2(0.0, 0.0));
 
     skyboxCount = pointsArray.length - skyboxStart;
+}
+
+
+function configureTexture(image)
+{
+    terrainTexture = gl.createTexture();
+
+    gl.bindTexture(gl.TEXTURE_2D, terrainTexture);
+
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+    gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.RGBA,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        image
+    );
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.uniform1i(gl.getUniformLocation(program, "terrainTexture"), 0);
+
+    useTexture = 1;
 }
 
 
@@ -212,19 +277,25 @@ window.onload = function init()
     if (!gl){ alert("WebGL isn't available"); }
 
     gl.viewport(0, 0, canvas.width, canvas.height);
+
     gl.clearColor(0.7, 0.9, 1.0, 1.0);
+
     gl.enable(gl.DEPTH_TEST);
 
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+
     gl.useProgram(program);
 
     generateTerrainData(terrainRows, terrainCols);
 
     buildTerrain();
+
     buildWater();
+
     buildSkybox();
 
-    var cBuffer = gl.createBuffer();
+
+    cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
 
@@ -232,16 +303,27 @@ window.onload = function init()
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
-    var vBuffer = gl.createBuffer();
+    tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+
+    var vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
+
+    vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-    
+
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
     modelView = gl.getUniformLocation(program, "modelView");
+
     projection = gl.getUniformLocation(program, "projection");
+
+    useTextureLoc = gl.getUniformLocation(program, "useTexture");
 
     document.getElementById("Button1").onclick =
         function() { radius *= 1.1; };
@@ -261,6 +343,25 @@ window.onload = function init()
     document.getElementById("Button6").onclick =
         function() { phi -= dr; };
 
+    document.getElementById("heightmapInput").onchange = function(event)
+    {
+        var file = event.target.files[0];
+
+        if (!file)
+        {
+            return;
+        }
+
+        var img = new Image();
+
+        img.onload = function()
+        {
+            configureTexture(img);
+        };
+
+        img.src = URL.createObjectURL(file);
+    };
+
     render();
 };
 
@@ -276,14 +377,22 @@ function render()
     );
 
     mvMatrix = lookAt(eye, at, up);
+
     pMatrix = ortho(left, right, bottom, ytop, near, far);
+
     gl.uniformMatrix4fv(modelView, false, flatten(mvMatrix));
     gl.uniformMatrix4fv(projection, false, flatten(pMatrix));
 
+    //draw skybox
+    gl.uniform1i(useTextureLoc, 0);
     gl.drawArrays(gl.TRIANGLES, skyboxStart, skyboxCount);
-
+    //draw terrain
+    gl.uniform1i(useTextureLoc, useTexture);
     gl.drawArrays(gl.TRIANGLES, terrainStart, terrainCount);
 
+    //draw water
+    gl.uniform1i(useTextureLoc, 0);
     gl.drawArrays(gl.TRIANGLES, waterStart, waterCount);
+
     requestAnimFrame(render);
 }
